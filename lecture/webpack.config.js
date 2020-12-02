32543863342960a046e36c121d9833aa88ce477e
김종환 <dondon9513@gmail.com>
2020-12-02 16:33:03 +0900

    completed 2-6

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -3,12 +3,16 @@ const path = require("path"); //node 깔려있으면 사용가능
 module.exports = {
   name: "word-relay-setting", //웹팩설정의 이름
   mode: "development", //실서비스 : production
-  devtool: "eval", //빠르게
+  devtool: "eval", //빠르게(개발), production일때는 hidden-source-map
+
+  resolve: {
+    extensions: [".jsx", ".js"], //확장자를 포함시킴으로 이 두 타입은 확장자 생략하여 entry작성 가능
+  },
 
   //entry에 있는 file을 읽고, module을 적용한 후 output에 뺀다.
   entry: {
     //app: ["./client.jsx", "./WordRelay.jsx"] 배열안의것들을 합쳐서 아래의 app.js로 만들어준다.
-    app: ["./client.jsx"], //client.jsx가 WordRelay.jsx를 불러오고있기 때문에 client.jsx만 포함시켜도 된다.
+    app: ["./client"], //client.jsx가 WordRelay.jsx를 불러오고있기 때문에 client.jsx만 포함시켜도 된다.
   }, // 입력
 
   module: {
