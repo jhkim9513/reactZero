6c087a04c2dcf96cd511bf43fe194e6bf02d48c4
김종환 <dondon9513@gmail.com>
2020-12-04 19:28:16 +0900

    completed 2-7

--- gugudan/webpack.config.js
+++ gugudan/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { webpack } = require("webpack");
 
 module.exports = {
   mode: "development",
@@ -16,12 +17,26 @@ module.exports = {
         test: /\.jsx?$/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                //자동으로 옛날 브라우저 지원
+                targets: {
+                  //원하는 브라우저에만 맞춰서 적용되게함
+                  browsers: ["> 10% in KR"],
+                },
+                debug: true,
+              },
+            ],
+            "@babel/preset-react",
+          ],
           plugins: [],
         },
       },
     ],
   },
+  // plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
   output: {
     filename: "app.js",
     path: path.join(__dirname, "dist"),
